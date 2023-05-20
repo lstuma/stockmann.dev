d5bbfc085170f106c798f5aac8dd228ae6d52d10
sdoxl <g.lstuma@gmail.com>
2023-05-20 23:53:30 +0200

    development v0.01e

--- global.js
+++ global.js
@@ -8,9 +8,13 @@ console.log("v0.1e (unstable)")
 
 const pre_content = document.querySelector('#pre-content')
 const content = document.querySelector('#content')
+const post_content = document.querySelector('#post-content')
 
 async function get_content(link) {
 	return await fetch(link).then(response => response.text());
 }
 
 // load in top navbar
+pre_content.innerHTML += await get_content('/templates/nav-top.html')
+// load in bottom footer
+post_content.innerHTML += await get_content('/templates/footer-bottom.html')
