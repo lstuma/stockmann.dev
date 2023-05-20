33d507127c1d4caecc6ad12ae8eba1d33e66524f
sdoxl <g.lstuma@gmail.com>
2023-05-21 00:13:28 +0200

    development v0.1h

--- global.js
+++ global.js
@@ -1,6 +1,8 @@
 //	js banner
 //
-console.log("v0.1g (unstable)")
+let version ="v0.1h"
+let stable = true
+console.log(version + stable?"":" (unstable)")
 
 //
 //	this script loads in base materials like navbar and footer..
@@ -11,17 +13,36 @@ const content = document.querySelector('#content')
 const post_content = document.querySelector('#post-content')
 
 async function get_content(link) {
-	return await fetch(link).then(response => response.text());
+	return await fetch(link).then(response => response.text())
 }
 
 //
 //	switching of page
 //
 async function load(link) {
-	window.history.pushState(link, "unused", link);
+	// update address bar
+	window.history.pushState(link, "unused", link)
+	// get data for actual content of page
+	let data = "404 - Not found!"
+	if(link.startsWith("/blog/") && link != '/blog/')
+		data = await req_blog(link)
+	else
+		data = await req_page(link)
+
+	// update content of page
+	content.innerHTML = data
 }
-
-
+// get content of page
+async function req_page(link) {
+	// get page
+	return await get_content(link+"index.html")
+}
+async function req_blog(link) {
+	// get raw data
+	let data = await get_content(link[-1]+".txt")
+	// format markdown
+	return data
+}
 
 
 //
@@ -33,4 +54,4 @@ async function setup() {
 	// load in bottom footer
 	post_content.innerHTML += await get_content('/templates/footer-bottom.html')
 }
-setup();
+setup()
