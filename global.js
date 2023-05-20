ac6ad26d35ba7142a4c581d594e913306b9c41fa
sdoxl <g.lstuma@gmail.com>
2023-05-20 23:23:18 +0200

    did stuff

---
+++ global.js
@@ -0,0 +1,14 @@
+//
+//	This script loads in base materials like  navbar and footer..
+//
+
+const pre_content = document.querySelector('#pre-content')
+const content = document.querySelector('#content')
+
+function get_content(link) {
+	let data = await fetch(link).then((response) => { return response})
+	return data;
+}
+
+// load in top navbar
+...
