feb05503ed9321881b5e73741cacb28ddef9abcd
lstuma <g.lstuma@gmail.com>
2023-10-24 21:32:51 +0200

    :wrench:

--- backend/http_cors.py
+++ backend/http_cors.py
@@ -3,10 +3,10 @@ import app.http_settings as http_settings
 
 def get_headers(request: JSONRequest):
     cors_headers = {
-        'Access-Control-Allow-Credentials' : http_settings.allow_credentials,
+        'Access-Control-Allow-Credentials' : str(http_settings.ALLOW_CREDENTIALS),
     }
-    if '*' in http_settings.allowed_origins:
+    if '*' in http_settings.ALLOWED_ORIGINS:
         cors_headers['Access-Control-Allow-Origin'] = '*'
-    elif request.headers('Origin') in http_settings.allowed_origins:
+    elif request.headers('Origin') in http_settings.ALLOWED_ORIGINS:
         cors_headers['Access-Control-Allow-Origin'] = request.headers('Origin')
     return cors_headers
