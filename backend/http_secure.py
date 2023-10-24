e9cd8aa5e1af70c31b23ee27d578e9fe36ac06b4
lstuma <g.lstuma@gmail.com>
2023-10-24 22:01:58 +0200

    :lock::bug:

--- backend/http_secure.py
+++ backend/http_secure.py
@@ -6,16 +6,17 @@ import ssl
 log = logger.create_log(name='HTTPSECURE', color='bpink')
 
 def make_secure(sock, hostname, cert_path, privatekey_path, password=None):
-    if password: pass_func = lambda: password
-    else: pass_func = ask_password
-    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-    try:
-        ssl_context.load_cert_chain(certfile=cert_path, keyfile=privatekey_path, password=pass_func)
-    except FileNotFoundError:
-        log(0, 'failed to secure socket', bypass=True)
-        log(0, 'cert_path or privatekey_path invalid!', bypass=True)
-        return None
-    secure_sock = ssl_context.wrap_socket(sock, server_side=True)
+    #if password: pass_func = lambda: password
+    #else: pass_func = ask_password
+    #ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    #try:
+    #    ssl_context.load_cert_chain(certfile=cert_path, keyfile=privatekey_path, password=pass_func)
+    #except FileNotFoundError:
+    #    log(0, 'failed to secure socket', bypass=True)
+    #    log(0, 'cert_path or privatekey_path invalid!', bypass=True)
+    #    return None
+    #secure_sock = ssl_context.wrap_socket(sock, server_side=True)
+    secure_sock = ssl.wrap_socket(sock, certfile=cert_path, keyfile=privatekey_path)
     return secure_sock
     
 def ask_password():
