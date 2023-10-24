dcc4a7491160cb9d5171c50425da28d4a3216d83
lstuma <g.lstuma@gmail.com>
2023-10-24 22:27:58 +0200

    :lock:

--- frontend/src/api/blog/fetchFire.js
+++ frontend/src/api/blog/fetchFire.js
@@ -1,11 +1,11 @@
 export const fetchFirestat = async (id) => {
-    let json = await fetch('http://api.stockmann.dev/get_fire?id='+id).then(data => data.text())
+    let json = await fetch('https://api.stockmann.dev/get_fire?id='+id).then(data => data.text())
     
     let data = JSON.parse(json)
     return data.fire
 }
 export const fetchVotefire = async (id) => {
-    let json = await fetch('http://api.stockmann.dev/vote_fire?id='+id).then(data => data.text())
+    let json = await fetch('https://api.stockmann.dev/vote_fire?id='+id).then(data => data.text())
     
     let data = JSON.parse(json)
 }
