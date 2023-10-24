03e24074ca63981698a2873def09c87a763d5b01
lstuma <g.lstuma@gmail.com>
2023-10-24 22:17:24 +0200

    :loud_volume:

--- backend/http_secure.py
+++ backend/http_secure.py
@@ -6,10 +6,15 @@ import ssl
 log = logger.create_log(name='HTTPSECURE', color='bpink')
 
 def make_secure(sock, hostname, cert_path, privatekey_path, password=None):
+    # privkey password-policy
     if not password: password = ask_password
+
+    # ssl context for wrapping socket (tls)
     ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
     try:
+        log(3, f'loading tls certificate from {cert_path}')
         ssl_context.load_verify_locations(cert_path)
+        log(3, f'loading tls private key from {privatekey_path}')
         ssl_context.load_cert_chain(certfile=cert_path, keyfile=privatekey_path, password=password)
     except FileNotFoundError:
         log(0, 'failed to secure socket', bypass=True)
